7eaf17e782a75ad674b42e1031bf1a9a0175757a
yoolbi <yoolbi716@gmail.com>
2024-04-26 11:20:48 +0900

    add: profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,63 @@
 import MenuProfile from "../components/menu/MenuProfile";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Button, ImageList, ImageListItem } from "@mui/material";
 
 const Profile = () => {
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+    },
+  ];
   return (
     <div style={{ width: "100%", height: "100vh", display: "flex" }}>
       <MenuProfile />
       <div
-        className="right"
+        className="rightProfile"
         style={{
           width: "100%",
           height: "100%",
@@ -14,7 +66,64 @@ const Profile = () => {
           alignItems: "center",
         }}
       >
-        Profile
+        <div
+          className="profileInfo"
+          style={{
+            borderBottom: "1px solid #c4c4c4",
+            width: "80%",
+            height: "200px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <AccountCircleIcon
+            style={{ width: "80px", height: "80px", marginRight: "20px" }}
+          />
+          <div>
+            <div>
+              <b>yoolbi</b>
+              <Button
+                variant="outlined"
+                size="small"
+                style={{ margin: "0px 30px 0px 50px" }}
+              >
+                팔로우 +
+              </Button>
+              <Button variant="outlined" size="small">
+                메시지 보내기
+              </Button>
+            </div>
+            <div style={{ display: "flex", marginTop: "15px" }}>
+              <div>게시물 {itemData.length}</div>
+              <div style={{ margin: "0px 30px" }}>
+                팔로워 <b style={{ cursor: "pointer" }}>10</b>
+              </div>
+              <div>
+                팔로잉 <b style={{ cursor: "pointer" }}>10</b>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "80%", height: "65%" }}>
+          <ImageList sx={{ width: "100%", height: "100%" }} cols={3} gap={20}>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  alt={item.title}
+                  loading="lazy"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    cursor: "pointer",
+                  }}
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </div>
       </div>
     </div>
   );
